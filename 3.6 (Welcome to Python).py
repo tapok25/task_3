ce604247015d9120c008892f618519d08ab76aab
Sergey <salnikovs802@gmail.com>
2022-10-26 19:28:45 +0700

    done

---
+++ 3.6 (Welcome to Python).py
@@ -0,0 +1,3 @@
+name = 'Ivan'
+surname = 'Ivanov'
+print(f'Hello {name} {surname}! You just delved into Python. Great start!')
---
+++ 3.6 (money).py
@@ -0,0 +1,3 @@
+amount = 312942.224
+# print('{0:,}'.format(amount)) второй вариант
+print(f'денежная сумма {amount:,}')
